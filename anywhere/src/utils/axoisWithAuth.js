fa2730643ba7c7a4bf9153fb387b5063d864ffd2
Bill Xiong <xiong.bill@yahoo.com>
2020-08-26 18:13:24 -0700

    added

--- anywhere/src/utils/axoisWithAuth.js
+++ anywhere/src/utils/axoisWithAuth.js
@@ -4,7 +4,7 @@ const axiosWithAuth = () => {
     const token = localStorage.getItem("token")
 
     return axios.create({
-        baseURL: "https://anywherefitnessapp.herokuapp.com/api",
+        baseURL: "",
         headers: {
             Authorization: token
         }
